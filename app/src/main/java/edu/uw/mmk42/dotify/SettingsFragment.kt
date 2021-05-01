6f0b38c308b9cb6938ef41d5a7ca84259d8443f8
mmk42 <mmk42@uw.edu>
2021-05-01 01:04:56 -0700

    step 5

---
+++ app/src/main/java/edu/uw/mmk42/dotify/SettingsFragment.kt
@@ -0,0 +1,39 @@
+package edu.uw.mmk42.dotify
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import edu.uw.mmk42.dotify.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment() {
+
+    private val navController by lazy {findNavController()}
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSettingsBinding.inflate(inflater)
+        with(binding) {
+            btnAbout.setOnClickListener{
+                navController.navigate(R.id.aboutFragment)
+                Toast.makeText(requireContext(), "You clicked on about button", Toast.LENGTH_SHORT).show()
+            }
+            btnProfile.setOnClickListener{
+                navController.navigate(R.id.profileFragment)
+                Toast.makeText(requireContext(), "You clicked on profile button", Toast.LENGTH_SHORT).show()
+            }
+            btnStatistics.setOnClickListener{
+                navController.navigate(R.id.statisticsFragment)
+                Toast.makeText(requireContext(), "You clicked on stats button", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return binding.root
+    }
+}

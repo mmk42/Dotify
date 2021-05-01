ca2461c9f89b35cd5728a5287def69ce97fc8b5a
mmk42 <mmk42@uw.edu>
2021-05-01 05:22:01 -0700

    pt10 done

--- app/src/main/java/edu/uw/mmk42/dotify/SongListActivity.kt
+++ app/src/main/java/edu/uw/mmk42/dotify/SongListActivity.kt
@@ -9,6 +9,8 @@ import com.ericchee.songdataprovider.Song
 import com.ericchee.songdataprovider.SongDataProvider
 import edu.uw.mmk42.dotify.databinding.ActivitySongListBinding
 
+private const val CURR_SONG_KEY = "CURR_SONG_KEY"
+
 class SongListActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySongListBinding
     private lateinit var currSong: Song
@@ -16,9 +18,18 @@ class SongListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_list)
+
+
         title = "All Songs"
 
         binding = ActivitySongListBinding.inflate(layoutInflater).apply { setContentView(root) }
+        if (savedInstanceState != null) {
+            currSong = savedInstanceState?.getParcelable(CURR_SONG_KEY)!!
+            with(binding) {
+                tvCurrSong.text = root.context.getString(R.string.curr_song_format, currSong.title,currSong.artist)
+                tvCurrSong.visibility = View.VISIBLE
+            }
+        }
 
         songs = SongDataProvider.getAllSongs()
         with(binding) {
@@ -49,16 +60,21 @@ class SongListActivity : AppCompatActivity() {
                 songs = newList
                 adapter.updateSongs(newList)*/
                 val newSongs = songs.toMutableList().apply {
-                    Log.i("mmk42-11", "position to delete is $position")
+                    //Log.i("mmk42-11", "position to delete is $position")
                     remove(song)
                 }
                 songs = newSongs
-                Log.i("mmk42-12", "position to delete is $position")
+                //Log.i("mmk42-12", "position to delete is $position")
                 adapter.updateSongs(newSongs)
                 Toast.makeText(this@SongListActivity, root.context.getString(R.string.remove_song_format, song.title), Toast.LENGTH_SHORT).show()
                 true
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putParcelable(CURR_SONG_KEY, currSong)
+        super.onSaveInstanceState(outState)
     }
 
 }
